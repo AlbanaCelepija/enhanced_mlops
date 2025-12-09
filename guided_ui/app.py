7d5f0f3427aa2317b3c5fe224cff3667514df3de
AlbanaCelepija <acelepija@fbk.eu>
2025-12-09 10:29:27 +0100

    refactor(ui)reorganise the layout and clean code

--- guided_ui/app.py
+++ guided_ui/app.py
@@ -1,62 +1,175 @@
 import os
+import sys
 import yaml
+import inspect
 import streamlit as st
+import importlib.util
 from streamlit_option_menu import option_menu
+from library.src.artifact_types import Data, Configuration, Report
 
-USE_CASES_FOLDER = "../framework/library/use_cases"
+current_folder = os.path.dirname(os.path.abspath(__file__))
+parent_folder = os.path.dirname(current_folder)
+USE_CASES_FOLDER = os.path.join(parent_folder,"framework/library/use_cases")
 use_cases_list = os.listdir(USE_CASES_FOLDER)
 platform_list = ["local", "dh"]
     
-def step_1(current_step: str = "Data preparation", selected_aspect: str = "Baseline", aipc_config_path: str = "aipc.yaml"):
-    st.title("📊 Stage 1: Data preparation")
-    option_menu(
-            "Operations",
-            ["Data profiling", "Data validation", "Data preprocessing", "Data documentation"],
-            menu_icon="sliders",
-            default_index=0,
-            orientation="horizontal"
+def import_from_path(module_name, file_path):
+    """Import a module given its name and file path."""
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)    
+    return module
+
+def stages_settings(stage):
+    if stage == "Data preparation":
+        st.title("📊 Stage 1: Data preparation")
+        selected_operation = option_menu(
+                "Operations",
+                ["Data profiling", "Data validation", "Data preprocessing", "Data documentation"],
+                menu_icon="sliders",
+                default_index=0,
+                orientation="horizontal"
+            )
+        selected_operation = selected_operation.replace(" ", "_").lower()
+        step_operations_module="data_preparation.py"
+        return selected_operation, step_operations_module
+    elif stage == "Modeling":
+        st.title("🛠️ Stage 2: Modeling")
+        selected_operation = option_menu(
+                "Operations",
+                ["Feature engineering", "Model training", "Model evaluation", "Model validation", "Model documentation"],
+                menu_icon="sliders",
+                default_index=0,
+                orientation="horizontal"
         )
-    with open(aipc_config_path, "r") as yaml_file:
+        selected_operation = selected_operation.replace(" ", "_").lower()
+        step_operations_module="modelling.py"
+        return selected_operation, step_operations_module
+    elif stage == "Operationalisation":
+        st.title("⚙️ Stage 3: Operationalization")   
+        selected_operation = option_menu(
+                "Operations",
+                ["Model deployment", "Model monitoring", "Production data monitoring", 
+                "System monitoring", "Pre inference transformations", "Post inference transformations"],
+                menu_icon="sliders",
+                default_index=0,
+                orientation="horizontal"
+            )  
+        selected_operation = selected_operation.replace(" ", "_").lower()
+        step_operations_module="operationalization.py"
+        return selected_operation, step_operations_module
+    
+def show_stage(current_step: str = "Data preparation", selected_aspect: str = "Baseline", current_product: str = "tabular", current_framework: str = "local"):
+    selected_operation, step_operations_module = stages_settings(current_step)
+    product_config_file = os.path.join(USE_CASES_FOLDER, current_product, "metadata", f"aipc_{current_framework}.yaml")
+    with open(product_config_file, "r") as yaml_file:
         aipc_configs = yaml.safe_load(yaml_file)
-    
-    with st.expander("Operation name 1", expanded=True):
-        tab1, tab2, tab3 = st.tabs(["Documentation", "Code", "Metadata"])
-        with tab1:
-            st.write("This is the Documentation tab")
-            code = st.text_area("Documentation", key="doc_1")
-        with tab2:
-            st.write("This is the Code tab")
-            code = st.text_area("Code implementation", key="code_1")
-        with tab3:
-            st.write("This is the Metadata tab")
-            metadata = st.text_area("Metadata information", key="meta_1")
-            
-        if st.button("Run operation", key="run_op_1"):
-            st.write("Running operation procedure...")
-            
-            
-    with st.expander("Operation name 2"):
-        tab1, tab2, tab3 = st.tabs(["Documentation", "Code", "Metadata"])
-        with tab1:
-            st.write("This is the Documentation tab")
-            code = st.text_area("Documentation", key="doc_2")
-        with tab2:
-            st.write("This is the Code tab")
-            code = st.text_area("Code implementation", key="code_2")
-        with tab3:
-            st.write("This is the Metadata tab")
-            metadata = st.text_area("Metadata information", key="meta_2")
-            
-        if st.button("Run operation", key="run_op_2"):
-            st.write("Running operation procedure...")
-    
+        # operations
+        operations = aipc_configs["operations"]
+        data_operations = [
+            elem for elem in operations if elem["stage"] == "data_preparation" and elem["requirement_dimension"] == selected_aspect.lower()
+        ]
+        model_operations = [
+            elem for elem in operations if elem["stage"] == "modelling" and elem["requirement_dimension"] == selected_aspect.lower()
+        ]
+        operationalisation_ops = [
+            elem for elem in operations if elem["stage"] == "operationalization" and elem["requirement_dimension"] == selected_aspect.lower()
+        ]
+        # artifacts
+        artifacts = aipc_configs["artifacts"]
+        data_artifacts = artifacts["data"]
+        model_artifacts = artifacts["model"]
+        configuration_artifacts = artifacts["configuration"]
         
+        #st.write(aipc_configs)
+        if current_step == "Data preparation":
+            populate_frames(data_operations, selected_operation, step_operations_module, current_product, current_framework, model_artifacts, data_artifacts, configuration_artifacts)
+        elif current_step == "Modeling":
+            populate_frames(model_operations, selected_operation, step_operations_module, current_product, current_framework, model_artifacts, data_artifacts, configuration_artifacts)
+        elif current_step == "Operationalisation":
+            populate_frames(operationalisation_ops, selected_operation, step_operations_module, current_product, current_framework, model_artifacts, data_artifacts, configuration_artifacts)
+                
+def populate_frames(operations, selected_data_operation, step_operations_module, current_product, current_framework, model_artifacts, data_artifacts, configuration_artifacts):
+    for ind, operation in enumerate(operations):
+        if operation["type"] == selected_data_operation:
+            operation_id = operation["id"]
+            operation_name = operation["name"]
+            method_name = operation["implementation"]["spec"]["method_name"]
+            st.write(method_name)
+            
+            with st.expander(f"Operation {operation_id}: {operation_name}", expanded=True):
+                tab1, tab2, tab3 = st.tabs(["Documentation", "Code", "Metadata"])
+                with tab1:
+                    st.write("This is the Documentation tab")
+                    code = st.text_area("Documentation", key=f"doc_{ind}", value=operation["name"])
+                with tab2:
+                    st.write("This is the Code tab")
+                    method_content = load_method_content(method_name, current_product, current_framework, step_operations_module)
+                    code = st.text_area("Code implementation", key=f"code_{ind}", value=method_content)
+                with tab3:
+                    st.write("This is the Metadata tab")
+                    metadata = st.text_area("Metadata information", key=f"meta_{ind}", value=operation["name"])
+                    
+                if st.button("Run operation", key=f"run_op_{ind}"):
+                    if "run_op" not in st.session_state:
+                        st.session_state.run_op = ""
+                    st.write(st.session_state.run_op)
+                    st.session_state.run_op = run_data_operation(operation, data_artifacts, model_artifacts, configuration_artifacts, current_product, current_framework)
+
     
-def step_2(current_step: str = "Modeling", selected_aspect: str = "Baseline", aipc_config_path: str = "aipc.yaml"):
+def load_method_content(method_name, current_product, current_framework="local", step_operations_module="data_preparation.py"):
+    product_operations_file = os.path.join(USE_CASES_FOLDER, current_product, "src", f"{current_framework}_platform", step_operations_module)
+    curr_module = import_from_path("curr_module", product_operations_file)
+    func = getattr(curr_module, method_name)
+    source_text = inspect.getsource(func)
+    return source_text
+                
+
+def run_data_operation(operation, data_artifacts, model_artifacts, config_artifacts, current_product, current_framework="local"):
+    product_operations_file = os.path.join(USE_CASES_FOLDER, current_product, "src", f"{current_framework}_platform", "data_preparation.py")
+    curr_module = import_from_path("curr_module", product_operations_file)
+    
+    specs = operation["implementation"]["spec"]
+    method_name = specs["method_name"]
+    inputs = specs["inputs"]
+    outputs = specs["outputs"]
+    input_vars = {}
+    for my_input in inputs:
+        input_name = list(my_input.values())
+        input_artifact = [
+            art for art in data_artifacts if art["name"] == input_name[0]
+        ]
+        if len(input_artifact) > 0:
+            artifact_vars = {
+                var_name: var_value
+                for var_name, var_value in input_artifact[0].items()
+                if var_name != "name"
+            }
+            input_vars.update({"data": Data(**artifact_vars)})
+        input_artifact = [
+            art for art in config_artifacts if art["name"] == input_name[0]
+        ]
+        if len(input_artifact) > 0:
+            artifact_vars = {
+                var_name: var_value
+                for var_name, var_value in input_artifact[0].items()
+                if var_name != "name"
+            }
+            input_vars.update({"config": Configuration(**artifact_vars)})
+    print(input_vars)
+
+    func = getattr(curr_module, method_name)
+    result = func(**input_vars)    
+    #method_name = globals()[method_name]
+    #result = method_name(**input_vars)
+    
+    
+def step_2(current_step: str = "Modeling", selected_aspect: str = "Baseline", current_product: str = "tabular", current_framework: str = "local"):
     st.title("🛠️ Stage 2: Modeling")
     option_menu(
             "Operations",
-            ["Feature Engineering", "Model training", "Model evaluation", "Model validation", "Model and product documentation"],
+            ["feature_engineering", "model_training", "model_evaluation", "model_validation", "model_aipc_documentation"],
             menu_icon="sliders",
             default_index=0,
             orientation="horizontal"
@@ -79,12 +192,12 @@ def step_2(current_step: str = "Modeling", selected_aspect: str = "Baseline", ai
                     st.metric(label=column_labels[i], value=f"{80 + i}%")
                     st.button(f"Show {column_labels[i]}", key=f"{i}")
 
-def step_3(current_step: str = "Operationalization", selected_aspect: str = "Baseline", aipc_config_path: str = "aipc.yaml"):
+def step_3(current_step: str = "Operationalization", selected_aspect: str = "Baseline", current_product: str = "tabular", current_framework: str = "local"):
     st.title("⚙️ Stage 3: Operationalization")    
     option_menu(
             "Operations",
-            ["Model deployment", "Model monitoring", "Production data monitoring", 
-             "System monitoring", "Pre-inference transformations", "Post-inference transformations"],
+            ["model_deployment", "model_monitoring", "production_data_monitoring", 
+             "system_monitoring", "pre_inference_transformations", "post_inference_transformations"],
             menu_icon="sliders",
             default_index=0,
             orientation="horizontal"
@@ -117,9 +230,8 @@ def main():
     st.sidebar.title("AI product development lifecycle")   
     if st.sidebar.button("Start new AI product", key="new_ai_product"):
         pass #st.sidebar.write("Creating new AI product project ...") 
-    current_frameowrk = st.sidebar.selectbox("Platform", use_cases_list)  
+    current_framework = st.sidebar.selectbox("Tool governance platform", platform_list)  
     current_product = st.sidebar.selectbox("AI Products list", use_cases_list)    
-    st.write(os.path.join(USE_CASES_FOLDER, current_product, "src", "data_preparation.py")) 
     #session_state_test()
          
     with st.sidebar:
@@ -139,15 +251,7 @@ def main():
             default_index=0,
             orientation="vertical"
         )
-    if current_step == "Data preparation":
-        step_1(current_step, selected_aspect)     
-    
-    elif current_step == "Modeling":
-        step_2(current_step, selected_aspect)
-        
-    elif current_step == "Operationalisation":
-        step_3(current_step, selected_aspect)
-    
+    show_stage(current_step, selected_aspect, current_product, current_framework)         
     
 
 if __name__ == "__main__":
